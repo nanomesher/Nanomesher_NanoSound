251e5e8e29da0e46887c5669b1f7104de231fec1
nanomesher <info@nanomesher.com>
2019-02-17 00:07:20 +0800

    fix build scripts

--- make_nanosound_lirc.cmd
+++ make_nanosound_lirc.cmd
@@ -1,1 +1,2 @@
 7z.exe -ttar a dummy .\nanosound_lirc\* -so | 7z.exe -si -tgzip a nanosound_lirc.tar.gz
+ren nanosound_lirc.tar.gz nanosound_lirc%date:~-4,4%%date:~-7,2%%date:~-10,2%.tar.gz
--- make_nanosound_oled.cmd
+++ make_nanosound_oled.cmd
@@ -1,1 +1,2 @@
 7z.exe -ttar a dummy .\nanosound_oled\ -so | 7z.exe -si -tgzip a nanosound_oled2.tar.gz
+ren nanosound_oled2.tar.gz nanosound_oled%date:~-4,4%%date:~-7,2%%date:~-10,2%.tar.gz
