06548b5c0b902aae290b6d3214861ee0047ee22c
nanomesher <info@nanomesher.com>
2022-01-16 18:39:30 +0800

    Create install_nanosound_for_v3.sh

---
+++ packages/install_nanosound_for_v3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/volumio
+rm -f nanosound_forv3.zip
+rm -rf nanosound
+wget https://github.com/nanomesher/Nanomesher_NanoSound/raw/master/packages/nanosound_forv3.zip
+mkdir ./nanosound
+miniunzip nanosound_forv3.zip -d ./nanosound
+cd ./nanosound
+volumio plugin install
+
+echo "Cleaning up..."
+cd /home/volumio
+rm -f nanosound_forv3.zip
+rm -rf nanosound
